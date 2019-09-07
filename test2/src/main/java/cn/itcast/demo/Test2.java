7f4592a8134e15dfba9e502c0b01220836435cfb
qiaqiawang <wwb03295477659>
2019-09-07 16:46:20 +0800

    王文博第一次测试

--- test2/src/main/java/cn/itcast/demo/Test2.java
+++ test2/src/main/java/cn/itcast/demo/Test2.java
@@ -6,5 +6,6 @@ public class Test2 {
         System.out.println("各位大佬来测试");
         System.out.println("刘周坤测试");
         System.out.println("王朋博来报道");
+        System.out.println("王文博修改");
     }
 }
