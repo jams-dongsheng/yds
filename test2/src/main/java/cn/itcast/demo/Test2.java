205d249139837812f344623083f2ca88adb41bb0
qiaqiawang <wwb03295477659>
2019-09-07 16:43:32 +0800

    Merge branch 'master' of F:\gitHost\yds with conflicts.

--- test2/src/main/java/cn/itcast/demo/Test2.java
+++ test2/src/main/java/cn/itcast/demo/Test2.java
@@ -1,10 +1,7 @@
 package cn.itcast.demo;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("各位大佬来测试");
-        System.out.println("王朋博来报道");
     }
 }
